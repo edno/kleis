669bc273037e90611fa0f39941d8be016d3c5157
edno <edno_gh@yahoo.fr>
2016-08-12 23:48:49 +0100

    remove demo data from seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(AccountsTableSeeder::class);
-        $this->call(GroupsTableSeeder::class);
     }
 }
