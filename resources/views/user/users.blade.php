73826318e1f88ce42aa51407d9f6fe46a00647f0
edno <edno_gh@yahoo.fr>
2016-09-11 19:38:56 +0200

    Add tooltip to status icon in Administators page

--- resources/views/user/users.blade.php
+++ resources/views/user/users.blade.php
@@ -114,7 +114,10 @@
                                     <div>{{ $user->email }}</div>
                                 </td>
                                 <td class="table-text">
-                                    <div><i class="fa {{ $user->getStatus()['icon'] }}" title="{{ $user->getStatus()['text'] }}"></i></div>
+                                    <div><i class="fa {{ $user->getStatus()['icon'] }} has-tooltip"
+                                        data-toggle="tooltip"
+                                        data-placement="bottom"
+                                        title="{{ $user->getStatus()['text'] }}"></i></div>
                                 </td>
                                 <td class="align-right col-xs-2">
                                     <div class="btn-toolbar">
