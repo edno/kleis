6a08b683289d58b7079fba7ba1a296c07fadbf36
edno <edno_gh@yahoo.fr>
2016-10-20 21:52:13 +0200

    Legal info are now conditional subview and linked to localization

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <div class="panel panel-default">
-        <div class="panel-heading">Connexion</div>
+        <div class="panel-heading">@lang('auth.login')</div>
         <div class="panel-body">
             <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                 {{ csrf_field() }}
 
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="col-md-4 control-label">Adresse email</label>
+                    <label for="email" class="col-md-4 control-label">@lang('auth.email')</label>
 
                     <div class="col-md-6">
                         <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
@@ -22,7 +22,7 @@
                 </div>
 
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="col-md-4 control-label">Mot de passe</label>
+                    <label for="password" class="col-md-4 control-label">@lang('auth.password')</label>
 
                     <div class="col-md-6">
                         <input id="password" type="password" class="form-control" name="password">
@@ -39,7 +39,7 @@
                     <div class="col-md-6 col-md-offset-4">
                         <div class="checkbox">
                             <label>
-                                <input type="checkbox" name="remember"> Rester connect&eacute;
+                                <input type="checkbox" name="remember"> @lang('auth.remember')
                             </label>
                         </div>
                     </div>
@@ -48,22 +48,18 @@
                 <div class="form-group">
                     <div class="col-md-6 col-md-offset-4">
                         <button type="submit" class="btn btn-primary">
-                            <i class="fa fa-btn fa-sign-in"></i> Connexion
+                            <i class="fa fa-btn fa-sign-in"></i> @lang('auth.login')
                         </button>
 
-                        <a class="btn btn-link" href="{{ url('/password/reset') }}">Mot de passe oubli&eacute; ?</a>
+                        <a class="btn btn-link" href="{{ url('/password/reset') }}">@lang('auth.forgotten')</a>
                     </div>
                 </div>
             </form>
         </div>
     </div>
-    <div class="panel panel-default">
-        <div class="panel-heading">Article 323-1 du Code P&eacute;nal fran&ccedil;ais</div>
-        <div class="panel-body">
-            <ul>
-                <li>Le fait d&apos;acc&eacute;der ou de se maintenir, frauduleusement, dans tout ou partie d'un syst&egrave;me de traitement automatis&eacute; de donn&eacute;es est puni de deux ans d&apos;emprisonnement et de 60 000 &euro; d&apos;amende.</li>
-                <li>Lorsqu&apos;il en est r&eacute;sult&eacute; soit la suppression ou la modification de donn&eacute;es contenues dans le syst&egrave;me, soit une alt&eacute;ration du fonctionnement de ce syst&egrave;me, la peine est de trois ans d&apos;emprisonnement et de 100 000 &euro; d&apos;amende.</li>
-            </ul>
-        </div>
-    </div>
+
+    @if (View::exists('auth.legal.'.App::getLocale()))
+        @include('auth.legal.'.App::getLocale())
+    @endif
+    
 @endsection
