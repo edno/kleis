099f0964890c1415cd0e43af063446aa4fc53f50
edno <edno_gh@yahoo.fr>
2016-10-22 09:43:59 +0200

    Add specific locale for Le Refuge (FR-REFUGE)

---
+++ resources/lang/fr-refuge/accounts.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Accounts/Account View Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'group'                => 'D&eacute;l&eacute;gation',
+    'message.empty.groups' => '<strong>Aucune d&eacute;l&eacute;gation disponible !</strong> Veuillez cr&eacute;er une d&eacute;l&eacute;gation avant de continuer.',
+
+];
---
+++ resources/lang/fr-refuge/groups.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Groups View Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'tooltip.group' => 'Nom de la d&eacute;l&eacute;gation',
+    'groups'        => 'D&eacute;l&eacute;gation|D&eacute;l&eacute;gations',
+    'message'       => [
+                        'empty'  => 'Aucune d&eacute;l&eacute;gation',
+                        'search' => '{0} Aucune d&eacute;l&eacute;gation trouv&eacute;e|{1} :number d&eacute;l&eacute;gation trouv&eacute;e|[2,Inf] :number d&eacute;l&eacute;gations trouv&eacute;es',
+                    ],
+
+];
