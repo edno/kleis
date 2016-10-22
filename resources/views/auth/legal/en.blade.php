3899a7f196c7f6db3f61fae9929295d517eef186
edno <edno_gh@yahoo.fr>
2016-10-22 13:41:00 +0200

    Add legal info for en (UK)

---
+++ resources/views/auth/legal/en.blade.php
@@ -0,0 +1,10 @@
+<div class="panel panel-default">
+    <div class="panel-heading">United Kingdom Computer Misuse Act 1990</div>
+    <div class="panel-body">
+        <ul>
+            <li>unauthorised access to computer material, punishable by 12 months' imprisonment (or 6 months in Scotland) and/or a fine "not exceeding level 5 on the standard scale" (since 2015, unlimited);</li>
+            <li>unauthorised access with intent to commit or facilitate commission of further offences, punishable by 12 months/maximum fine (or 6 months in Scotland) on summary conviction and/or 5 years/fine on indictment;</li>
+            <li>unauthorised modification of computer material, punishable by 12 months/maximum fine (or 6 months in Scotland) on summary conviction and/or 10 years/fine on indictment;</li>
+        </ul>
+    </div>
+</div>
